e21c30ee826eb52459d8630b9df46f2f994b86bd
dmittrey <dmitryzubakhin@mail.ru>
2021-10-27 19:05:15 +0300

    validate functions SR

--- src/main/webapp/js/validate_functions.js
+++ src/main/webapp/js/validate_functions.js
@@ -1,42 +1,39 @@
-function validateForm(print_permission) {
+function validateForm() {
 
-    let Y_value = $('#Y_value').val();
     let X_value = $('#X_value').val();
+    let Y_value = $('#Y_value').val();
     let R_value = $('#R_value').val();
 
-    let info = "";
-    info += "<span>" + validateY(Y_value) + "</span>";
-    info += "<span>" + validateX(X_value) + "</span>";
-    info += "<span>" + validateR(R_value) + "</span>";
-
-    if (print_permission) {
-        $('.Error_text').html(info);
-    }
-
-    return (info === "<span></span>".repeat(3));
+    return validateButtonExist(X_value) &&
+        validateXRange(X_value) &&
+        validateTextExist(Y_value) &&
+        validateTextForm(Y_value) &&
+        validateYRange(Y_value) &&
+        validateTextExist(R_value) &&
+        validateTextForm(R_value) &&
+        validateRRange(R_value);
 }
 
-function validateY(field) {
-
-    if (!(field.trim() === "")) {
-        if (/^(0$|-?[1-9]\d*(\.\d*[1-9]$)?|-?0\.\d*[1-9])$/.test(field)) {
-            if ((parseInt(field) > -3) && (parseInt(field) < 3)) {
-                return "";
-            } else return "Координата Y задается числом в промежутке (-3...3)!\n";
-        } else return "Координата Y задается числом!\n";
-    } else return "Не введена координата Y!\n";
+function validateTextExist(field) {
+    return !(field.trim() === "");
 }
 
-function validateX(field) {
-
-    if (field === null) {
-        return "Не введена координата X!\n";
-    } else return "";
+function validateButtonExist(field) {
+    return !(field === null);
 }
 
-function validateR(field) {
+function validateTextForm(field) {
+    return (/^(0$|-?[1-9]\d*(\.\d*[1-9]$)?|-?0\.\d*[1-9])$/).test(field);
+}
 
-    if (field === null) {
-        return "Не введена координата R!\n";
-    } else return "";
+function validateXRange(field) {
+    return (parseInt(field) > -3) && (parseInt(field) < 3);
+}
+
+function validateYRange(field) {
+    return (parseInt(field) > -3) && (parseInt(field) < 3);
+}
+
+function validateRRange(field) {
+    return (parseInt(field) > -3) && (parseInt(field) < 3);
 }
